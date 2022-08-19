62f606ff4bde83a64bfba3cde68ac182e0b4d4d0
adisonsud <adison.chire.broncano@gmail.com>
2022-08-19 16:11:15 -0500

    label e input del nombre

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,12 +4,23 @@ const Formulario = () => {
       <h2 className="font-black text-3xl text-center">
         Seguimiento Pacientes</h2>
       
-      <p className="text-lg mt-5">
+      <p className="text-lg mt-5 text-center mb-10">
         Añade Pacientes y {""}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
       
-        
+      <form className="bg-white shadow-xl rounded-lg px-5 py-10">
+        <div>
+          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+          <input 
+            id="mascota"
+            type="text" 
+            placeholder="Nombre de la Mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+      </form>
+           
     </div>
   )
 }
