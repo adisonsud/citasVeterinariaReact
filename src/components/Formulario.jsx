fdf8b2f23ef46beb732b80745c03767235d141ce
adisonsud <adison.chire.broncano@gmail.com>
2022-08-24 18:23:48 -0500

    validar form y mensaje error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,30 @@
+import { useState, useEffect } from "react"
+
 const Formulario = () => {
+  const [nombre, setNombre] =useState('');
+  const [propietario, setPropietario] =useState('');
+  const [email, setEmail] =useState('');
+  const [alta, setAlta] =useState('');
+  const [sintomas, setSintomas] =useState('');
+  
+  const [error, setError] =useState(false);
+  // paso 1 - para enviar formulario 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    /* VALIDACION DE FORMULARIO */
+    /* .includes('') -> verifica q haya algun string vacio */
+    if ([nombre, propietario, email, alta, sintomas].includes('') ) {
+      console.log('Hay al menos un campo vacio');
+      setError(true) /*  */
+      return;
+    }else{
+      setError(false)
+    }
+  }
+  
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">
         Seguimiento Pacientes</h2>
       
@@ -9,7 +33,19 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold">Adminístralos</span>
       </p>
       
-      <form className="bg-white shadow-xl rounded-lg px-5 py-10 mb-10">
+      <form 
+      // parte 2- para ecribir en el form y este haga visible lo que se va escribiendo
+      onSubmit={handleSubmit}
+      className="bg-white shadow-xl rounded-lg px-5 py-10 mb-10"
+      >
+        {/* si error es True mostrar el mensaje de div */}
+        {error && 
+        <div className="bg-red-900 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+          <p>si hay un error</p>
+        </div>
+        
+        }
+        
         {/* Nombre de la Mascota*/}
         <div className="mb-5"> 
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
@@ -17,7 +53,14 @@ const Formulario = () => {
             id="mascota"
             type="text" 
             placeholder="Nombre de la Mascota"
-            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            /** onChange Es como un event Listener para ecribir en el form y este
+             * haga visible lo que se va escribiendo
+             */
+            value={nombre}
+            onChange = { (e) => setNombre(e.target.value) }
+            />
+            
         </div> {/* Fin - Nombre de la Mascota */}
         
         {/* Nombre del propietario */}
@@ -28,7 +71,12 @@ const Formulario = () => {
             id="propietario"
             type="text" 
             placeholder="Nombre del Propietario"
-            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={propietario}
+            onChange= {(e) => setPropietario(e.target.value)}
+            />
+            
+            
         </div> {/* Fin - Nombre del propietario */}
         
         {/* Email del propietario */}
@@ -39,7 +87,11 @@ const Formulario = () => {
             id="email"
             type="email" 
             placeholder="Email Contacto Propietario"
-            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange = {(e) => setEmail(e.target.value)}
+            />
+       
         </div> {/* Fin - Email del propietario */}
         
         {/* Fecha de Alta */}
@@ -49,18 +101,25 @@ const Formulario = () => {
           <input 
             id="alta"
             type="date" 
-            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={alta}
+            onChange = {(e) => setAlta(e.target.value)}        
+            />
+            
         </div> {/* Fin - Fecha de Alta */}
         
         {/* Sintomas de la Mascota */}
         <div className="mb-5">
           <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
-            Síntomas</label>
+            Síntomas
+          </label>
           <textarea 
           id="sintomas" 
           className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-          placeholder="Describe los Síntomas">
-          </textarea>
+          placeholder="Describe los Síntomas"
+          value = {sintomas}
+          onChange = { (e) => setSintomas(e.target.value)}
+          />
         </div> {/* Fin - Sintomas de la Mascota */}
         
         <input 
